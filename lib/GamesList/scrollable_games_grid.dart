9f337f3f86e336823a19959e55ce3b6f3607a153
alekla0126 <alekla0126@gmail.com>
2025-02-07 22:38:07 +0300

    Latest flutter dependencies

--- lib/GamesList/scrollable_games_grid.dart
+++ lib/GamesList/scrollable_games_grid.dart
@@ -1,4 +1,4 @@
-import 'package:chess_clock/Screens/second_Screen.dart';
+import 'package:chess_clock/Screens/second_screen.dart';
 import 'package:chess_clock/GlobalFiles/appState.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
